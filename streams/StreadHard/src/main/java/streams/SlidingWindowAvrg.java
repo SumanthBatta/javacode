6e67ebaa4b044fbe64df7122dcfe03771da233f6
SumanthBatta <battasumanth1091@gmail.com>
2025-11-11 11:47:16 +0530

    sliding window arvg

---
+++ streams/StreadHard/src/main/java/streams/SlidingWindowAvrg.java
@@ -0,0 +1,26 @@
+package streams;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class SlidingWindowAvrg {
+    public static void main(String[] args) {
+        List<Integer> input = Arrays.asList(4,8,15,16,23,42);
+
+        int window = 3;
+
+        List<Double> result = IntStream.range(0,input.size()-(window-1))
+                .mapToObj(i->input.subList(i, i+window))
+                .map(w->w.stream()
+                        .mapToInt(Integer::intValue)
+                        .average().
+                        orElse(0.0)
+                ).toList();
+
+        System.out.println(result);
+
+
+    }
+}
